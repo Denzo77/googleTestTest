31b93e54c714c3a9f0c73470b6ace5cefc8a99ac
Deniz Erbilgin <denzo@denzo-desktop>
2016-10-07 17:38:18 +0100

    Testing OTRadioLink

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 
 #include <stdint.h>
 #include <gtest/gtest.h>
+#include <utility/OTV0P2BASE_Util.h>
+#include <utility/OTV0P2BASE_Util.cpp>
 
 
 class ExampleClass
@@ -19,7 +21,7 @@ public:
 	uint8_t doSomething(uint8_t input)
 	{
 		myVal += input;
-		return (input + 10);
+		return (input + 20);
 	}
 
 	uint32_t myVal;
@@ -33,8 +35,19 @@ TEST(ExampleTest, ShouldAddTen)
 
 	//run tests
 	EXPECT_EQ(0, myClass.myVal);
-	EXPECT_EQ(1, myClass.myVal);  // If an EXPECT_ statement fails, the test carries on.
-	EXPECT_EQ(13, myClass.doSomething(3));
-	ASSERT_EQ(0, myClass.myVal);  // If an ASSERT_ statement fails, the function exits.
+//	EXPECT_EQ(1, myClass.myVal);  // If an EXPECT_ statement fails, the test carries on.
+	EXPECT_EQ(23, myClass.doSomething(3));
+//	ASSERT_EQ(0, myClass.myVal);  // If an ASSERT_ statement fails, the function exits.
 	EXPECT_EQ(3, myClass.myVal);
+}
+
+
+// Actually test a real OTRadioLink file.
+TEST(LibraryTest,ShouldConvertFromHex)
+{
+    const char s[] = "0a";
+    // This works. It's inline and only in the header.
+    EXPECT_EQ(10, OTV0P2BASE::parseHexDigit(s[1]));
+    // The compiler can't find this for some reason (function def in source file).
+    EXPECT_EQ(10, OTV0P2BASE::parseHexByte(s));
 }
